c63d661924f8ad17fb7c4618ce50e373f9221aae
Xaviou <x.psoud@gmail.com>
2018-02-05 10:52:35 +0100

    wxPython version of the AboutBoxDlg snippet

---
+++ AboutBoxDlg/Python/DlgAbout.py
@@ -0,0 +1,49 @@
+import wx
+
+class DlgAbout(wx.Dialog):
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, title=wx.GetStockLabel(wx.ID_ABOUT, wx.STOCK_NOFLAGS))
+
+        self._createInterface()
+
+        self.CenterOnParent()
+
+    def _createInterface(self):
+
+        sTitle = 'AboutBoxDlg (v1.0.0)'
+        sMsg = 'Made with wxPython ' + wx.version() + '\nBased on ' + wx.GetLibraryVersionInfo().VersionString
+        szrMain = wx.BoxSizer(wx.VERTICAL)
+
+        szrTop = wx.BoxSizer(wx.HORIZONTAL)
+
+        bmpCtrl = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap('../../common/wxWidgets.png', wx.BITMAP_TYPE_PNG))
+        szrTop.Add(bmpCtrl, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+
+        szrRight = wx.BoxSizer(wx.VERTICAL)
+
+        label = wx.StaticText(self, wx.ID_STATIC, sTitle)
+        fntTitle = label.GetFont()
+        fntTitle.MakeLarger()
+        fntTitle.MakeBold()
+        label.SetFont(fntTitle)
+        szrRight.Add(label, 0, wx.ALL|wx.ALIGN_CENTER, 5)
+
+        label = wx.StaticText(self, wx.ID_STATIC, 'Copyright (c) X.P. 2018')
+        szrRight.Add(label, 0, wx.LEFT|wx.RIGHT|wx.BOTTOM|wx.ALIGN_CENTER, 5)
+
+        label = wx.StaticText(self, wx.ID_STATIC, 'wxWidgets Application')
+        szrRight.Add(label, 0, wx.LEFT|wx.RIGHT|wx.TOP|wx.ALIGN_CENTER, 5)
+
+        label = wx.StaticText(self, wx.ID_STATIC, sMsg, style=wx.ST_NO_AUTORESIZE|wx.ALIGN_CENTER)
+        szrRight.Add(label, 0, wx.ALL|wx.EXPAND, 5)
+
+        szrTop.Add(szrRight, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+
+        szrMain.Add(szrTop, 0, wx.ALL, 5)
+
+        btnSzr = self.CreateSeparatedButtonSizer(wx.CLOSE)
+        szrMain.Add(btnSzr, 0, wx.ALL|wx.EXPAND, 5)
+
+        self.SetSizer(szrMain)
+
+        szrMain.SetSizeHints(self)
---
+++ AboutBoxDlg/Python/MainFrame.py
@@ -0,0 +1,53 @@
+import wx
+from DlgAbout import DlgAbout
+
+class MainFrame(wx.Frame):
+
+    def __init__(self):
+        wx.Frame.__init__(self, None, title=wx.GetApp().GetAppName())
+        self.SetIcon(wx.Icon('../../common/wxwin.ico'))
+
+        self._createControls()
+
+        self._connectControls()
+
+    def _createControls(self):
+        # A Statusbar in the bottom of the window
+        self.CreateStatusBar(1)
+        sMsg = 'wxPython ' + wx.version() + ' - ' + wx.GetLibraryVersionInfo().VersionString
+        self.SetStatusText(sMsg)
+
+        # Add a panel to the frame (needed under Windows to have a nice background)
+        pnl = wx.Panel(self, wx.ID_ANY)
+
+        # Create the menu bar
+        menuBar = wx.MenuBar()
+        # File menu
+        menuFile = wx.Menu()
+        menuFile.Append(wx.ID_EXIT)
+        menuBar.Append(menuFile, wx.GetStockLabel(wx.ID_FILE))
+        # Help menu
+        menuHelp = wx.Menu()
+        menuHelp.Append(wx.ID_ABOUT)
+        menuBar.Append(menuHelp, wx.GetStockLabel(wx.ID_HELP))
+        # Assigne the menubar
+        self.SetMenuBar(menuBar)
+
+        # On OS X, delete the created menus
+        # as the About and Exit items are handled by the OS specific menu
+        if wx.GetOsDescription()[:8] == 'Mac OS X':
+            menuBar.Remove(1)
+            del(menuHelp)
+            menuBar.Remove(0)
+            del(menuFile)
+
+    def _connectControls(self):
+        self.Bind(wx.EVT_MENU, self._onMenuExitClicked, id=wx.ID_EXIT)
+        self.Bind(wx.EVT_MENU, self._onMenuAboutClicked, id=wx.ID_ABOUT)
+
+    def _onMenuExitClicked(self, evt):
+        self.Close()
+
+    def _onMenuAboutClicked(self, evt):
+        dlg = DlgAbout(self)
+        dlg.ShowModal()
